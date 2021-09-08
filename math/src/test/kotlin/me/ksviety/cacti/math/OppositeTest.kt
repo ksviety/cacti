2fb25d224d1e4c50ae2b150c5365b9d3c29d49a6
ksviety <43730934+ksviety@users.noreply.github.com>
2021-09-08 06:32:20 +0500

    Added Opposite

---
+++ math/src/test/kotlin/me/ksviety/cacti/math/OppositeTest.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2021 ksviety
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package me.ksviety.cacti.math
+
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class OppositeTest {
+
+	@Test
+	fun `test opposite turns negative to positive`() {
+		val origin = Random.nextDouble(-100.0, -10.0)
+
+		assertEquals(
+			-origin,
+			Opposite(
+				Number(origin)
+			).collapse()
+		)
+	}
+
+	@Test
+	fun `test opposite turns positive to negative`() {
+		val origin = Random.nextDouble(10.0, 100.0)
+
+		assertEquals(
+			-origin,
+			Opposite(
+				Number(origin)
+			).collapse()
+		)
+	}
+}
